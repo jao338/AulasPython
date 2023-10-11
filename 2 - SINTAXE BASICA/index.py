d4d4659c1bde9b759fba4893fb1dfdc0b286e004
Jão <joaohenriquerc123@gmail.com>
2023-10-10 23:02:16 -0300

    2 - Sintaxe básica

---
+++ 2 - SINTAXE BASICA/index.py
@@ -0,0 +1,19 @@
+#   Comentário de linha única
+
+"""
+    Comentário de múltiplas linhas
+"""
+
+var = 'João'
+var = 1
+var = 'A' 
+var = 1.2
+
+#   Todas as variáveis recebem o mesmo valor
+num1=num2=num3 = 2
+
+#   Caso queira concatenar valores, use vírgula como separador. 
+print(var , " agora é um valor do tipo do")
+
+print(num1 + num2 + num3)
+print(num1,num2,num3)
